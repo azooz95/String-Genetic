396cbab1f34a431f4c533944fa609c0005a371df
azooz95 <azooz95@example.com>
2021-09-12 00:10:49 +0800

    adjusting the function

--- g.py
+++ g.py
@@ -14,11 +14,14 @@ class Binary():
         return np.random.randint(2, size=(number,no_binary))
 
     def fittness(self, solution, c_solution):
-        return sum(c_solution)/sum(solution) * 100 
+        solution = np.array(solution)
+        c_solution = np.array(c_solution)
+        a = np.extract(solution == c_solution, c_solution)
+        return sum(a)/len(solution)
 
 
 class Strng():
     pass
 
 a = Genetic()
-print(a.init_population(6))
+print(a.fittness([1,0,1,1] , [1,1,0,1]))
