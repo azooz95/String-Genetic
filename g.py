85d32f1a71e327caa10fd818668485e1c97759f8
azooz95 <azooz95@example.com>
2021-09-11 09:41:54 +0800

    init_population is added

--- g.py
+++ g.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Genetic():
     def __new__(self, binary = True, strng=False):
         self.binary = binary
@@ -8,11 +10,11 @@ class Genetic():
             return Binary()
 
 class Binary():
-    def population(self, population):
-        return "this is the population: " + population
+    def init_population(self, number, no_binary = 4):
+        return np.random.randint(2, size=(number,no_binary))
 
 class Strng():
     pass
 
 a = Genetic()
-print(a.population('hi'))
+print(a.init_population(6))
