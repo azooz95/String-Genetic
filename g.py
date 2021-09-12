bb2b85e9657e3480154c848041ea7834721daa65
azooz95 <azooz95@example.com>
2021-09-12 11:51:45 +0800

    adding init

--- g.py
+++ g.py
@@ -10,8 +10,16 @@ class Genetic():
             return Binary()
 
 class Binary():
+    def __init__(self) -> None:
+        self.all_data = []
+
     def init_population(self, number, no_binary = 4):
-        return np.random.randint(2, size=(number,no_binary))
+        self.all_data = np.random.randint(2, size=(number,no_binary))
+        return self.all_data 
+
+    def adding_todata(self, data):
+        self.all_data = np.concatenate((self.all_data, data), axis=0)
+        return self.all_data
 
     def fittness(self, solution, c_solution):
         solution = np.array(solution)
@@ -19,6 +27,11 @@ class Binary():
         a = np.extract(solution == c_solution, c_solution)
         return sum(a)/len(solution)
 
+    def selection(self):
+        pass
+
+    def crossover(self, f_par, s_par, singl_point = 2):
+        
 
 class Strng():
     pass
